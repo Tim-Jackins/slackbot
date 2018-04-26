55f4aadd2a52cb98370a4acb1c5a85658a47f614
Tim-Jackins <timmins.jack595@gmail.com>
2018-04-26 11:33:21 -0400

    Change setup of print_rank.py

--- print_rank.py
+++ print_rank.py
@@ -1,15 +1,18 @@
 import requests			# Allows you to send HTTP/1.1 requests
 from bs4 import BeautifulSoup	# BeautifulSoup is an awesome html parser that is useful to learn
 
-team = input('Enter a 2017 ZR alliance team: ')
-url = 'http://zerorobotics.mit.edu/tournaments/28/rank/100/0/'
-html_content = requests.get(url).content.decode('utf-8').strip() # Get the html page with requests
+def get_rank(team):
+	url = 'http://zerorobotics.mit.edu/tournaments/28/rank/100/0/'
+	html_content = requests.get(url).content.decode('utf-8').strip() # Get the html page with requests
 
-leaderboard = BeautifulSoup(html_content, 'lxml').body.table # Get the first table from the body of the page
+	leaderboard = BeautifulSoup(html_content, 'lxml').body.table # Get the first table from the body of the page
 
-cells = [str(elem) for elem in leaderboard.find_all('td')] # Find all occurances of <td> tag
+	cells = [str(elem) for elem in leaderboard.find_all('td')] # Find all occurances of <td> tag
 
-for i in range(len(cells)):
-	if i % 5 == 0:
-		if cells[i][64:-9] == team:
-			print('{0}\'s rank is {1}'.format(team, int(i / 5) + 1))
+	for i in range(len(cells)):
+		if i % 5 == 0:
+			if cells[i][64:-9] == team:
+				return '{0}\'s rank is {1}'.format(team, int(i / 5) + 1)
+	return 'Couldn\'t find team.'
+
+print(get_rank('BACON HORIZON ASP')
